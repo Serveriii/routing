f0e01211114285ee18db47ce66a2319871e492e7
Serveriii <n2jose00@students.oamk.fi>
2023-02-02 12:02:05 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import  Home  from "./Components/Home";
+import  About  from "./Components/About";
+import Contact from "./Components/Contact";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import NotFund from "./Components/NotFund";
+import { Route, Routes } from "react-router-dom";
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <Header/>
+    <div className="container"> 
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contactus" element={<Contact/>} />
+      <Route path="/*" element={<NotFund/>} />
+    </Routes>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+        <div className='container'>
+            <span>Place sticky footer here</span>
+        </div>
+    </footer>
+  )
+}
